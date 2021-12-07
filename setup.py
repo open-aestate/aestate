bc2e8a18f787b9e03d77193ed72ccb47af589100
cctvadmin <2075383131@qq.com>
2021-12-07 15:11:56 +0800

    remove web package

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 packages = ['aestate', 'aestate.ajson', 'aestate.ajson.sim', 'aestate.dbs', 'aestate.dbs._mssql', 'aestate.dbs._mysql',
-            'aestate.exception', 'aestate.libs', 'aestate.opera', 'aestate.opera.DBPool', 'aestate.util', 'aestate.web',
+            'aestate.exception', 'aestate.libs', 'aestate.opera', 'aestate.opera.DBPool', 'aestate.util',
             'aestate.web.http', 'aestate.work', 'aestate.work.sql', 'aestate.work.xmlhandler']
 print(setuptools.find_packages())
 
